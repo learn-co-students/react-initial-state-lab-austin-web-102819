21dd565f5f6d485de95bda2521b18f52209d6c6f
Yining Wang <yining.wang@utexas.edu>
2020-01-08 11:32:03 -0600

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,31 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends React.Component {
+	// we use the constructor to set the INITIAL STATE
+	constructor(props) {
+		super();
+		this.state = {
+			secondsLeft: props.initialCount
+		};
+	}
+
+	// our increment method makes use of the 'setState' method, which is what we use to alter state
+	// increment = () => {
+	// 	const newCount = this.state.count + 1;
+	// 	this.setState({
+	// 		count: newCount
+	// 	});
+	// };
+
+	render() {
+		// return <div onClick={this.increment}>{this.state.count}</div>;
+		return (
+			<div>
+				{this.state.secondsLeft === 0
+					? 'Boom!'
+					: `${this.state.secondsLeft} seconds left before I go boom!`}
+			</div>
+		);
+	}
+}
